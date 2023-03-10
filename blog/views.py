c13575a5d1228076ca6548fa06e6bb857733bb5c
Ragini Dhanuk <kumariragini9034@gmail.com>
2023-03-10 18:28:48 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    author = 'Waxllium Ladrian'
+    age = 49
+    address = 'The Citadel'
+    ctx= {'athr':author,'age':age, 'addr':address}
+    return render(request,'home.html',context=ctx)
+
+def categories(request):
+    cats = ['python', 'Django', 'Web Development','programming']
+    return render(request,'categories.html',
+                  context={'categories':cats})
+
+def articles(request):
+ data = [
+    {'title': 'First Article',
+     'author':'Waxllium Ladrian',
+       'date':'2020-01-01'},
+       {'title': 'The Basin of Life',
+        'author':'Steris',
+       'date':'2022-01-02' },
+       {'title':'The Second Article',
+        'author':'Waxllium Ladrian',
+        'date':'2020-01-03'},
+    ]
+     return render(request,'articles.html',
+              context={'articles':data})
